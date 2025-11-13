bb6fff7392fdb0b3f98a2763873946193d595edd
Sbubby1 <jcorreia1@email.neit.edu>
2025-11-13 10:25:16 -0500

    i tried i guess

--- cards.js
+++ cards.js
@@ -1,4 +1,12 @@
 export class cards {
-    
-    
+constructor() {
+    this.canvas = document.getElementById("myCanvas");
+
+    this.pencil = this.canvas.getContext("2d");
+
+    this.pencil.fillStyle = "red";
+    this.pencil.drawRect(50, 50, 150, 30)
+  }
+
+  
 }
--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 import { Toolbox } from "./toolbox.js";
-
+import { cards } from "./cards.js";
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 let toolbox = new Toolbox();
